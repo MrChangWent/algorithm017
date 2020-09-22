7f8486797d072e1cb99ed54593fc27352f626ef8
ZhangWentao <paddyzhang2018@163.com>
2020-09-22 23:49:12 +0800

    climbing-stairs, container-with-most-water(repair indentation)

--- Week_01/container-with-most-water.py
+++ Week_01/container-with-most-water.py
@@ -9,13 +9,13 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-		res, left, right = 0, 0, len(height) - 1
-		while left < right:
-			area = min(height[left], height[right]) * (right - left)
-			res = max(res, area)
-			if height[left] < height[right]:
-				left += 1
-			else:
-				right -= 1
-		return res
+        res, left, right = 0, 0, len(height) - 1
+        while left < right:
+            area = min(height[left], height[right]) * (right - left)
+            res = max(res, area)
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return res
 
